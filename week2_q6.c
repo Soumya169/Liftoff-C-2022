f1be45b6f6d11b02f6ec47b8594c27487c712679
Soumya169 <99471313+Soumya169@users.noreply.github.com>
2022-03-06 21:15:06 +0530

    Create week2_q6.c

---
+++ week2_q6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of N");
+    scanf("%d", &n);
+    for(int i = 1; i<=n; i++)
+    {
+    for(int j = 1; j<=n; j++)
+    {
+     if((j==1||j==n)||(i==1||i==n))
+     {
+    printf("*");
+    }
+    else{
+    printf(" ");
+    }
+   }
+  printf("\n");
+    }
+    return 0;
+}
