9e7a26db76aeb6c23432072ffd5938ae213f1bee
Soumya169 <99471313+Soumya169@users.noreply.github.com>
2022-03-06 19:59:45 +0530

    Create week2_q1.c

---
+++ week2_q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str1[30], str2[30]; 
+    printf("Enter a String:\n");  
+    gets(str1); 
+    printf("Enter another String:\n");  
+    gets(str2);
+    printf("The string after concatenation is:\n");
+    strcat(str1, str2);
+    puts(str1);
+    printf("The string after reversing is:\n");
+    strrev(str1);
+    puts(str1);
+    return 0;
+}
