529105d8832495f41fb4b4fe743ae8952e6aa7cf
Soumya Ranjan Maharana <99471313+Soumya169@users.noreply.github.com>
2022-05-02 19:38:04 +0530

    Update week3-q1.c

--- week3-q1.c
+++ week3-q1.c
@@ -2,7 +2,7 @@
 int hcf(int n1, int n2);
 int main() {
     int n1, n2;
-    printf("Enter two positive integers: ");
+    printf("enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
     printf("GCD of %d and %d is %d.", n1, n2, hcf(n1, n2));
     return 0;
